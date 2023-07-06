4bbaeca27aac34418ab6e5ebb950e0f0061f100f
Gagandeep Jayaprakash <gagandeepgowda7@gmail.com>
2023-07-06 21:29:15 +0100

    Refactored login route and HTML template: Moved HTML template to a separate file Updated login route logic for database queries Improved code organization

--- facebook/app.py
+++ facebook/app.py
@@ -1,6 +1,16 @@
+import mysql.connector
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+def establish_connection():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="GaganGagan@@1211",
+        database="gagandb1"
+    )
+    return connection
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -8,16 +18,45 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        # Check username and password against your database
-        # Replace this with your actual database check
+        connection = establish_connection()
+        cursor = connection.cursor()
 
-        # Assuming a hardcoded username and password for simplicity
-        if username == 'admin' and password == 'password':
+        # Check if the users table exists, if not create it
+        cursor.execute("SHOW TABLES LIKE 'users'")
+        table_exists = cursor.fetchone()
+        if not table_exists:
+            cursor.execute("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL)")
+
+        # Query the database to check username and password
+        query = "SELECT * FROM users WHERE username = %s AND password = %s"
+        cursor.execute(query, (username, password))
+
+        user = cursor.fetchone()
+
+        if user:
             return 'Login successful'
         else:
             return 'Login failed'
     else:
         return render_template('index.html')
+
+@app.route('/insert', methods=['POST'])
+def insert_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        connection = establish_connection()
+        cursor = connection.cursor()
+
+        # Insert user into the users table
+        query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+        cursor.execute(query, (username, password))
+        connection.commit()
+
+        return 'User inserted successfully'
+
+    return 'Invalid request'
 
 if __name__ == '__main__':
     app.run(debug=True)
